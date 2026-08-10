d457bd62e1b1226c8e1f52f359a33cc7326c7cac
PIYUSH PAL <piyushpalc@gmail.com>
2026-08-10 20:59:48 +0530

    good output

--- cal.py
+++ cal.py
@@ -21,4 +21,4 @@ elif operator == "/":
 else:
     result = "Invalid operator"
 
-print("first number", num1, "second number", num2, "operator", operator, "=", "result", result)
+print("first number operator second number", num1, operator,num2, "=", "result", result)
